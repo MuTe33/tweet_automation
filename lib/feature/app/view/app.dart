718a8570f4602692325d6d0fe39380ae935e8f0e
MuTe33 <48680353+MuTe33@users.noreply.github.com>
2022-05-12 14:02:37 +0200

    update color

--- lib/feature/app/view/app.dart
+++ lib/feature/app/view/app.dart
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
         colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+          accentColor: const Color(0xFF13B9FD),
         ),
       ),
       localizationsDelegates: const [
